fbcab46ddd55c13248c489189614bb275cddcbed
Heinz-Alexander Fuetterer <fuetterh@posteo.de>
2023-10-20 19:54:24 +0200

    style: update exceptions

--- src/oaipmh_scythe/exceptions.py
+++ src/oaipmh_scythe/exceptions.py
@@ -1,86 +1,44 @@
-"""
-    oaipmh_scythe.oaiexceptions
-    ~~~~~~~~~~~~~~~~~~~~
+# SPDX-FileCopyrightText: 2015 Mathias Loesch
+#
+# SPDX-License-Identifier: BSD-3-Clause
 
-    OAI errors.
-
-    :copyright: Copyright 2015 Mathias Loesch
-"""
+"""Custom exceptions for OAI-PMH related errors."""
 
 
 class BadArgument(Exception):
-    """
-    The request includes illegal arguments, is missing required arguments,
-    includes a repeated argument, or values for arguments have an illegal
-    syntax.
-    """
-
-    pass
+    """The request includes illegal arguments, is missing required arguments, includes a repeated argument,
+    or values for arguments have an illegal syntax."""
 
 
 class BadVerb(Exception):
-    """
-    Value of the verb argument is not a legal OAI-PMH verb, the verb argument
-    is missing, or the verb argument is repeated.
-    """
-
-    pass
+    """Value of the verb argument is not a legal OAI-PMH verb, the verb argument is missing,
+    or the verb argument is repeated."""
 
 
 class BadResumptionToken(Exception):
-    """
-    The value of the resumptionToken argument is invalid or expired.
-    """
-
-    pass
+    """The value of the resumptionToken argument is invalid or expired."""
 
 
 class CannotDisseminateFormat(Exception):
-    """
-    The metadata format identified by the value given for the metadataPrefix
-    argument is not supported by the item or by the repository.
-    """
-
-    pass
+    """The metadata format identified by the value given for the metadataPrefix argument
+    is not supported by the item or by the repository."""
 
 
 class IdDoesNotExist(Exception):
-    """
-    The value of the identifier argument is unknown or illegal in this
-    repository.
-    """
-
-    pass
+    """The value of the identifier argument is unknown or illegal in this repository."""
 
 
 class NoSetHierarchy(Exception):
-    """
-    The repository does not support sets.
-    """
-
-    pass
+    """The repository does not support sets."""
 
 
 class NoMetadataFormat(Exception):
-    """
-    There are no metadata formats available for the specified item.
-    """
-
-    pass
+    """There are no metadata formats available for the specified item."""
 
 
 class NoRecordsMatch(Exception):
-    """
-    The combination of the values of the from, until, set and metadataPrefix
-    arguments results in an empty list.
-    """
-
-    pass
+    """The combination of the values of the from, until, set and metadataPrefix arguments results in an empty list."""
 
 
 class OAIError(Exception):
-    """
-    Context specific OAI errors not covered by the classes above.
-    """
-
-    pass
+    """Context specific OAI errors not covered by the classes above."""
