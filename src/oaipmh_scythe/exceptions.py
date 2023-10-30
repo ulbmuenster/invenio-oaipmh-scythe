ddba030a8d04621f92612f67ca3ca4faefeab1e5
Heinz-Alexander Fuetterer <fuetterh@posteo.de>
2023-10-30 20:41:20 +0100

    refactor: add custom base exception
    Fixes: #49

--- src/oaipmh_scythe/exceptions.py
+++ src/oaipmh_scythe/exceptions.py
@@ -5,40 +5,44 @@
 """Custom exceptions for OAI-PMH related errors."""
 
 
-class BadArgument(Exception):
+class OAIPMHError(Exception):
+    """Base exception for all OAI-PMH related errors."""
+
+
+class BadArgument(OAIPMHError):
     """The request includes illegal arguments, is missing required arguments, includes a repeated argument,
     or values for arguments have an illegal syntax."""
 
 
-class BadVerb(Exception):
+class BadVerb(OAIPMHError):
     """Value of the verb argument is not a legal OAI-PMH verb, the verb argument is missing,
     or the verb argument is repeated."""
 
 
-class BadResumptionToken(Exception):
+class BadResumptionToken(OAIPMHError):
     """The value of the resumptionToken argument is invalid or expired."""
 
 
-class CannotDisseminateFormat(Exception):
+class CannotDisseminateFormat(OAIPMHError):
     """The metadata format identified by the value given for the metadataPrefix argument
     is not supported by the item or by the repository."""
 
 
-class IdDoesNotExist(Exception):
+class IdDoesNotExist(OAIPMHError):
     """The value of the identifier argument is unknown or illegal in this repository."""
 
 
-class NoSetHierarchy(Exception):
+class NoSetHierarchy(OAIPMHError):
     """The repository does not support sets."""
 
 
-class NoMetadataFormat(Exception):
+class NoMetadataFormat(OAIPMHError):
     """There are no metadata formats available for the specified item."""
 
 
-class NoRecordsMatch(Exception):
+class NoRecordsMatch(OAIPMHError):
     """The combination of the values of the from, until, set and metadataPrefix arguments results in an empty list."""
 
 
-class OAIError(Exception):
+class OAIError(OAIPMHError):
     """Context specific OAI errors not covered by the classes above."""
